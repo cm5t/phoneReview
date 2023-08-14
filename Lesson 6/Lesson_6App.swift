13e5fa8dacc28f91449d6df044ec5f598b0a0cd1
Dylan <dylan_kwok_heng_yi@s2023.ssts.edu.sg>
2023-08-14 15:53:07 +0800

    Initial Commit

---
+++ Lesson 6/Lesson_6App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_6App.swift
+//  Lesson 6
+//
+//  Created by Dylan Kwok Heng Yi on 14/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
